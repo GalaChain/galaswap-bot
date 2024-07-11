602cfee8656aee7dab309effc7d54c6fedd2c269
Randall <randallschmidt1@gmail.com>
2024-07-11 11:41:55 -0400

    normalize signatures

--- src/dependencies/galaswap/galachain_signing.ts
+++ src/dependencies/galaswap/galachain_signing.ts
@@ -1,3 +1,4 @@
+import BN from 'bn.js';
 import ellipticPkg from 'elliptic';
 import jsSha3Pkg from 'js-sha3';
 import stringify from 'json-stringify-deterministic';
@@ -22,12 +23,21 @@ export function signObject<TInputType extends object>(
   const keccak256Hash = Buffer.from(keccak256.digest(stringToSignBuffer));
   const privateKeyBuffer = Buffer.from(privateKey.replace(/^0x/, ''), 'hex');
 
-  const signature = Buffer.from(ecSecp256k1.sign(keccak256Hash, privateKeyBuffer).toDER()).toString(
-    'base64',
-  );
+  const signature = ecSecp256k1.sign(keccak256Hash, privateKeyBuffer);
+
+  // Normalize the signature if it's greater than half of order n
+  if (signature.s.cmp(ecSecp256k1.curve.n.shrn(1)) > 0) {
+    const curveN = ecSecp256k1.curve.n;
+    const newS = new BN(curveN).sub(signature.s);
+    const newRecoverParam = signature.recoveryParam != null ? 1 - signature.recoveryParam : null;
+    signature.s = newS;
+    signature.recoveryParam = newRecoverParam;
+  }
+
+  const signatureString = Buffer.from(signature.toDER()).toString('base64');
 
   return {
     ...toSign,
-    signature,
+    signature: signatureString,
   };
 }
